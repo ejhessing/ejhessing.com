017066301f507e38486a9d2ae477f786aed80d47
Erwin Hessing <ejhessing@gmail.com>
2023-03-24 23:29:51 +1300

    Spinning ball

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,19 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import ThreeScene from "./spinning";
+import SpinningBall from "./spinning";
 
 const Home: NextPage = () => {
   return (
     <div className="text-3xl font-bold flex justify-center items-center h-screen bg-[#1da1f2] text-white">
       <div className="flex flex-col w-full h-full justify-center items-center flex-1">
-        <div className="mt-20">{"Heya, I'm EJ"}</div>
-        <div className="">
-          <ThreeScene />
+        <div className="w-full h-full flex">
+          <div className="flex flex-col w-1/3 h-full justify-center items-center flex-1 bg-gray-800">
+            {"Heya, I'm EJ"}
+          </div>
+          <div className="w-1/2 h-1/2">
+            <SpinningBall id={Date.now()} />
+          </div>
         </div>
       </div>
     </div>
